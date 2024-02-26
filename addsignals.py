c2dac389528382bf6d1413048fe77d266853a15b
MariaBec4 <159092161+MariaBec4@users.noreply.github.com>
2024-02-26 00:07:14 -0500

    Update addsignals.py

--- addsignals.py
+++ addsignals.py
@@ -8,7 +8,7 @@ data1, sampling_rate1 = librosa.load(rootFolder + pathname1, sr = 44100)
 pathname2 = "SmithBechwati-chirp.wav"
 data2, sampling_rate2 = librosa.load(rootFolder + pathname2, sr = 44100)
 
-outPathname = "SmithBechwait-quick_brown_fox_chirp.wav"
+outPathname = "SmithBechwati-quick_brown_fox_chirp.wav"
 if (sampling_rate1!=sampling_rate2):
     print("Sampling Rates are not equal!")
 

e4eb6b28010bbdc5b8794782d2b648926b4915b7
MariaBec4 <159092161+MariaBec4@users.noreply.github.com>
2024-02-28 22:58:36 -0500

    Update addsignals.py
    It was unclear in the instructions if we had to add the sine tone or the chirp, so I made it so it was both

--- addsignals.py
+++ addsignals.py
@@ -5,12 +5,23 @@ rootFolder = "Sound Recordings/"
 pathname1 = "SmithBechwati-quick_brown_fox.wav"
 data1, sampling_rate1 = librosa.load(rootFolder + pathname1, sr = 44100)
 
+#Adding Chirp
 pathname2 = "SmithBechwati-chirp.wav"
 data2, sampling_rate2 = librosa.load(rootFolder + pathname2, sr = 44100)
 
-outPathname = "SmithBechwati-quick_brown_fox_chirp.wav"
+outPathname1 = "SmithBechwati-quick_brown_fox_chirp.wav"
 if (sampling_rate1!=sampling_rate2):
     print("Sampling Rates are not equal!")
 
 else:
-    write(rootFolder + outPathname, sampling_rate1, data1+data2) # sampling rates are same so selecting either is fine
+    write(rootFolder + outPathname1, sampling_rate1, data1+data2) # sampling rates are same so selecting either is fine
+
+#Adding Sine Tone
+pathname3 = "SmithBechwati-sinetone.wav"
+data3, sampling_rate3 = librosa.load(rootFolder + pathname3, sr = 44100)
+outPathname2 = "SmithBechwati-quick_brown_fox_sinetone.wav"
+if (sampling_rate1!=sampling_rate2):
+    print("Sampling Rates are not equal!")
+
+else:
+    write(rootFolder + outPathname2, sampling_rate1, data1+data2)
